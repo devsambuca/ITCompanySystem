155096dc34fa4c0c61871ea558ef4381f4876807
Vladimir Fominykh <stoyakovi4@gmail.com>
2018-01-15 12:22:57 +0300

    Added class DevViewer

--- src/main/java/net/devsambuca/Main.java
+++ src/main/java/net/devsambuca/Main.java
@@ -1,15 +1,11 @@
 package net.devsambuca;
 
 import net.devsambuca.view.ViewEntity;
-import net.devsambuca.view.ViewMenu;
 
 public class Main {
 
     public static void main(String[] args) {
-        ViewEntity developerView = new ViewEntity();
-        ViewMenu viewMenu = new ViewMenu();
-
-        viewMenu.run();
-        developerView.run();
+        ViewEntity viewEntity = new ViewEntity();
+        viewEntity.run();
     }
 }
--- src/main/java/net/devsambuca/view/ViewEntity.java
+++ src/main/java/net/devsambuca/view/ViewEntity.java
@@ -1,39 +1,17 @@
 package net.devsambuca.view;
 
-import net.devsambuca.controller.DevController;
-import net.devsambuca.model.Developer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 
 public class ViewEntity {
     boolean exit;
-    DevController devController = new DevController();
 
     public void run() {
-        while (!exit) {
             showMenu();
-            int choice1 = getInputEntity();
-            performAction(choice1);
+            int choice = getInputEntity();
+            performAction(choice);
         }
-    }
-
-    private int getInputEntity() {
-        int choice1 = -1;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (choice1 < 0 || choice1 > 6) {
-            try {
-                System.out.println("\nEnter your choice: ");
-                choice1 = Integer.parseInt(reader.readLine());
-            } catch (NumberFormatException e) {
-                System.out.print("Entity not found. Please try again.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return choice1;
-    }
 
     private void performAction(int choice) {
         switch (choice) {
@@ -42,110 +20,48 @@ public class ViewEntity {
                 System.out.println("Thanks for using our application");
                 break;
             case 1:
-                showAll();
+                DevViewer devViewer = new DevViewer();
+                devViewer.run();
                 break;
-            case 2:
-                create();
-                break;
-            case 3:
-                read();
-                break;
-            case 4:
-                update();
-                break;
-            case 5:
-                delete();
-                break;
+//            case 2:
+//                read();
+//                break;
+//            case 3:
+//                update();
+//                break;
+//            case 4:
+//                delete();
+//                break;
             default:
                 System.out.println("An unknown error has occured.");
         }
     }
 
-    private void showAll(){
-        List<Developer> dev = devController.getAll();
-        for (Developer d1 : dev)
-            System.out.println(d1);
-    }
-
-    private void read() {
-        while (true) {
+    private int getInputEntity() {
+        int choice = -1;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (choice < 0 || choice > 6) {
             try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-                System.out.println("\nInput ID developer: ");
-                devController.read(Long.parseLong(reader.readLine()));
-                System.out.println(reader);
-
+                System.out.println("\nEnter your choice: ");
+                choice = Integer.parseInt(reader.readLine());
             } catch (NumberFormatException e) {
-                System.out.print("Invalid selection. Please try again.");
+                System.out.print("Entity not found. Please try again.");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return;
         }
+        return choice;
     }
 
-    private void create() {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Input ID: ");
-            Developer developer = new Developer();
-            developer.setId(Long.parseLong(reader.readLine()));
-            System.out.println("Input firstname: ");
-            developer.setFirstName(reader.readLine());
-            System.out.println("Input lastname: ");
-            developer.setLastName(reader.readLine());
-            System.out.println("Input specialty: ");
-            developer.setSpecialty(reader.readLine());
-           System.out.println("Input salary: ");
-//            developer.setSkills();
-            devController.create(developer);
-            System.out.println(developer);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void update() {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Input ID: ");
-            Developer developer = new Developer();
-            developer.setId(Long.parseLong(reader.readLine()));
-            System.out.println("Input firstname: ");
-            developer.setFirstName(reader.readLine());
-            System.out.println("Input lastname: ");
-            developer.setLastName(reader.readLine());
-            System.out.println("Input specialty: ");
-            developer.setSpecialty(reader.readLine());
-           System.out.println("Input salary: ");
-//            developer.setSkills(reader.readLine());
-            devController.update(developer);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void delete(){
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Input ID developer: ");
-            devController.delete(Long.parseLong(reader.readLine()));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     private void showMenu() {
         System.out.println("\nPlease make a selection");
-        System.out.println("1. Show all");
-        System.out.println("2. read");
-        System.out.println("3. create");
-        System.out.println("4. update");
-        System.out.println("5. delete");
+        System.out.println("1. Developer");
+        System.out.println("2. Skill");
+        System.out.println("3. Team");
+        System.out.println("4. Project");
+        System.out.println("5. Company");
+        System.out.println("6. Customer");
         System.out.println("0. Exit");
     }
 }
